ec5663effbd508e6673084007d15598f5ec1283b
Spencer Barton <sbarton272@gmail.com>
2015-02-09 17:43:34 -0500

    Added hough lines

--- app/src/main/java/edhyah/com/qbot/SpencerActivity.java
+++ app/src/main/java/edhyah/com/qbot/SpencerActivity.java
@@ -12,11 +12,17 @@ import org.opencv.android.CameraBridgeViewBase.CvCameraViewListener2;
 import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
 import org.opencv.core.Mat;
+import org.opencv.imgproc.Imgproc;
 
 
 public class SpencerActivity extends Activity implements CvCameraViewListener2 {
 
     private static final String TAG = "SpencerActivity";
+    private static final double HOUGH_RHO = 1;
+    private static final double HOUGH_THETA = Math.PI / 180;
+    private static final int HOUGH_THRESH = 10;
+    private static final double HOUGH_MIN_LEN = 15;
+    private static final double HOUGH_MAX_LINE_GAP = 90;
     private PortraitCameraView mOpenCvCameraView;
 
     private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
@@ -76,6 +82,13 @@ public class SpencerActivity extends Activity implements CvCameraViewListener2 {
 
     @Override
     public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+
+        // Find lines
+        Mat lines = new Mat();
+        Imgproc.HoughLinesP(inputFrame.gray(), lines, HOUGH_RHO, HOUGH_THETA, HOUGH_THRESH,
+                HOUGH_MIN_LEN, HOUGH_MAX_LINE_GAP);
+        Log.i(TAG, lines.toString());
+
         return inputFrame.rgba();
     }
 }
